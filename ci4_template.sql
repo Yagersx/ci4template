31f362c0ec12be7693033410f4403e00b28f191e
Yagersx <imartinezlopez.contacto@gmail.com>
2023-04-27 13:02:51 -0600

    chore: Se agrega .env.example, ci4_template.sql y public/dist

---
+++ ci4_template.sql
@@ -0,0 +1,78 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.11.2-MariaDB-log - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para ci4_db
+CREATE DATABASE IF NOT EXISTS `ci4_template` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
+<<<<<<< HEAD
+<<<<<<< HEAD
+USE `ci4_template`;
+=======
+USE `ci4_db`;
+>>>>>>> 0c7eb8a (parent 39d4cc89a83ff2d58a327dc13997ee96274dd90a)
+=======
+USE `ci4_template`;
+>>>>>>> 210b57e (test)
+
+-- Volcando estructura para tabla ci4_db.employees
+CREATE TABLE IF NOT EXISTS `employees` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL DEFAULT '0',
+  `address` varchar(100) DEFAULT NULL,
+  `salary` int(11) DEFAULT 0,
+  `role_id` int(11) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `confirmed` tinyint(1) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `role_id` (`role_id`) USING BTREE,
+  CONSTRAINT `FK1_employees_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3164 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Volcando datos para la tabla ci4_db.employees: ~1 rows (aproximadamente)
+INSERT INTO `employees` (`employee_id`, `name`, `last_name`, `birthday`, `email`, `phone`, `address`, `salary`, `role_id`, `password`, `token`, `confirmed`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+	(1, 'Admin', 'CI4', NULL, 'admin@ci4domain.com', '1234567891', 'ci4Dir', 0, 1, '$2y$12$2jw6Ylmv99SCyEVTtm5YEulcUVxV9lH1tNg2yAtWQuWQ10E6rGf.6', NULL, 1, '2023-04-24 22:26:17', 1, '2023-04-27 18:58:43', 1, NULL, NULL);
+
+-- Volcando estructura para tabla ci4_db.roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `is_admin` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`role_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- Volcando datos para la tabla ci4_db.roles: ~1 rows (aproximadamente)
+INSERT INTO `roles` (`role_id`, `description`, `created_at`, `updated_at`, `deleted_at`, `is_admin`) VALUES
+	(1, 'Administrador', '2023-04-24 00:33:09', NULL, NULL, b'1');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
